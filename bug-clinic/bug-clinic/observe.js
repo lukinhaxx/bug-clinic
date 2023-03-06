0e7464d983860935ec5beccfd73306eeed74cef9
lukinhaxx <126528237+lukinhaxx@users.noreply.github.com>
2023-03-06 20:46:24 -0300

    Add files via upload

---
+++ bug-clinic/bug-clinic/observe.js
@@ -0,0 +1,15 @@
+
+
+module.exports = scenario
+
+var replify = require('replify')
+
+function scenario(callback) {
+    var createServer = require('http').createServer
+    var server = createServer()
+
+    server.__message = 'REPLs are neat'
+    var repl = replify({ name: 'bug-clinic' }, server)
+
+    server.listen(8080, function () { callback(server, repl) })
+}
